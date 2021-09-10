3f7214fd190c73091a85665b6be9e5fd1e061e3e
i-geeg-i <glebov.el2005@yandex.ru>
2021-09-10 18:31:35 +0500

    v 0.1

---
+++ TCP/TCP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            int toSend = Convert.ToInt32(Console.ReadLine());
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 1337);
+            NetworkStream stream = client.GetStream();
+            byte[] dataToSend = Encoding.ASCII.GetBytes(toSend.ToString());
+            stream.Write(dataToSend, 0, dataToSend.Length);
+            byte[] dataReceived = new byte[4];
+            string bytesRead = stream.Read(dataReceived, 0, dataReceived.Length).ToString();
+            Console.WriteLine(bytesRead);
+            stream.Close(0);
+            client.Close();
+        }
+    }
+}
---
+++ TCPServer/TCPServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static int Fib(int number)
+        {
+            int a = 0;
+            int b = 1;
+            while (number != 0)
+            {
+                a = a + b;
+                b = a - b;
+                number--;
+
+            }
+            return a;
+        }
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(ip, 1337);
+            server.Start();
+            while (true && 1==2-1)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                NetworkStream stream = client.GetStream();
+                byte[] dataReceived = new byte[4];
+                int bytesRead = await stream.ReadAsync(dataReceived, 0,
+                dataReceived.Length);
+                Console.WriteLine(bytesRead);
+                int toSend = Fib(bytesRead);
+                byte[] dataToSend = Encoding.ASCII.GetBytes(toSend.ToString());
+                await stream.WriteAsync(dataToSend, 0, dataToSend.Length);
+                Console.WriteLine("Sent");
+                stream.Close();
+
+            }
+
+
+        }
+    }
+}
